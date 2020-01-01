03ebdc889f68ff3cca1ccfbdded4cffba8a5ae88
tmorovati <t.morovati.99@gmail.com>
2020-01-01 12:39:10 -0800

    read serial data from usb port (COM3)

--- test.cpp
+++ test.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main()
 {
-	HANDLE hCom; // global variable, serial handle
-	hCom = CreateFile("COM1", //COM1 port
-		GENERIC_READ | GENERIC_WRITE, //Allow read and write
-		0, // Exclusive
-		NULL,
-		OPEN_EXISTING, //Open rather than create
-		FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, //overlay
-		NULL);
-	If(hCom == INVALID_HANDLE_VALUE)
-	{
-		AfxMessageBox("Open COM failed!");
-		Return FALSE;
-	}
-	Return TRUE;
+	//HANDLE hCom; // global variable, serial handle
+	//hCom = CreateFile("COM1", //COM1 port
+	//	GENERIC_READ | GENERIC_WRITE, //Allow read and write
+	//	0, // Exclusive
+	//	NULL,
+	//	OPEN_EXISTING, //Open rather than create
+	//	FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, //overlay
+	//	NULL);
+	//If(hCom == INVALID_HANDLE_VALUE)
+	//{
+	//	AfxMessageBox("Open COM failed!");
+	//	Return FALSE;
+	//}
+	//Return TRUE;
 }
